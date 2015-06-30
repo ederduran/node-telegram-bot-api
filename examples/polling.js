40b645c0b4e47b3a59074cd0f777ff75f077ccf6
Eder Durán <ederduran@eduranexperienciasxcaretcommx.local>
2015-06-30 18:16:13 -0500

    bot api

--- examples/polling.js
+++ examples/polling.js
@@ -5,21 +5,21 @@ var options = {
   polling: true
 };
 
-var token = 'YOUR_TELEGRAM_BOT_TOKEN';
+var token = '';
 
 var bot = new TelegramBot(token, options);
 bot.getMe().then(function (me) {
-  console.log('Hi my name is %s!', me.username);
+  console.log('Hi my name is Tucky an ', me.username, 'from Xcaret Park');
 });
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
   if (msg.text == '/photo') {
     // From file
     var photo = __dirname+'/../test/bot.gif';
-    bot.sendPhoto(chatId, photo, {caption: "I'm a bot!"});
+    bot.sendPhoto(chatId, photo, {caption: "I'm a bot!", action: "upload_photo"});
   }
-  if (msg.text == '/audio') {
-    var url = 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg';
+  if (msg.text == '/loveme') {
+    var url = 'http://mp3falls.com/a/cache/Lil_Wayne_-_Love_Me%201.mp3';
     // From HTTP request!
     var audio = request(url);
     bot.sendAudio(chatId, audio)
@@ -31,6 +31,18 @@ bot.on('message', function (msg) {
   }
   if (msg.text == '/help') {
     var opts = {reply_to_message_id: msg.message_id};
-    bot.sendMessage(chatId, 'This is only a test :D', opts);
+    bot.sendMessage(chatId, "Test some methods: \n/photo to recieve a photo \n/loveme to get an audio \nand /help to... well i don't know");
+  }
+  if (msg.text == '/start') {
+    var opts = {reply_to_message_id: msg.message_id};
+
+    bot.sendMessage(chatId, "Hi my name is Tucky, an Master Bot from Xcaret Park, i am currently in development, please be patient! Test some methods: \n/photo to recieve a photo \n/loveme to get an audio \nand /help to... well i don't know");
+
+  }
+  if (msg.text == '/promotions') {
+    var opts = {reply_to_message_id: msg.message_id};
+
+    bot.sendMessage(chatId, "These are the promotions we have right nows");
+
   }
 });
